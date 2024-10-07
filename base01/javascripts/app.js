9f209d042a151a61bcc3acb6dba997c01a2181a6
SHOHEL HOSSAIN <developershohel@yahoo.com>
2024-10-07 17:13:40 +0600

    javascript class two

--- base01/javascripts/app.js
+++ base01/javascripts/app.js
@@ -1,46 +1,58 @@
-// Get an element
-// How many way we get an element
-const method1 = document.getElementById("root");
-const method2 = document.getElementsByTagName("div");
-const method3 = document.getElementsByClassName("update-components-carousel-job__content");
-const method4 = document.getElementsByName("name");
-const method5 = document.querySelector("#name");
-const method6 = document.querySelectorAll("[type=email]");
-const method7 = document.querySelectorAll("input");
-console.log(method1);
-console.log(method2);
-console.log(method3);
-console.log(method4);
-console.log(method5);
-console.log(method6);
+// Single Element select
+const singleElementOne = "boss";
+let index = 10;
+var mixValue = "ok";
+console.log(singleElementOne);
+console.log(index);
+console.log(mixValue);
 
-method1.style.backgroundColor = "red";
-// method2.style.color = 'blue' // app.js:17 Uncaught TypeError: Cannot set properties of undefined (setting 'color') at app.js:17:21
-method2[0].style.color = "blue"; // NO error
-console.log(method2.length);
-for (let index = 0; index < method2.length; index++) {
-    const element = method2[index];
-    console.log(element)
-    element.style.border = '1px solid black'
+function optonal(params) {
+    const singleElementTwo = "boss 2";
+    let indexTwo = 5;
+    var mixValueTwo = "ok 2";
+    console.log(singleElementTwo);
+    console.log(indexTwo);
+    console.log(mixValueTwo);
 }
-console.log(typeof method3);
-const method3type = Object.entries(method3);
-Object.entries(method3).forEach((element) => {
-  // element => {} es6 method
-  console.log(element);
-});
-console.log(method3type);
-const list = ["ok", "boss"];
-list.forEach((element) => {
-  // element => {} es6 method
-  console.log(element);
-});
 
-Object.entries(method3).map((element, i) => {
-  console.log(element, i);
-});
-Object.values(method3).map(function (element, i) {
-      console.log(element, i);
-});
+optonal();
 
+function withoutParams() {
+    const singleElementOne = "boss 3";
+    index = 7;
+    let mixValue = "ok 3";
+    console.log(singleElementOne);
+    index = 9;
+    console.log(index);
+    console.log(mixValue);
+}
+console.log(index);
+withoutParams();
+
+
+// Single element
+const singleValueOne = document.getElementById('name')
+const singleValueTwo = document.querySelector('#input')
+console.log(singleValueOne)
+console.log(singleValueTwo)
+// Always select one value
+// javascript apply function and style only single value
+
+// All elements 
+const allElementOne = document.getElementsByTagName('input')
+const allElementTwo = document.getElementsByClassName('any class name')
+const allElementThree = document.getElementsByName('password')
+const allElementFour = document.querySelectorAll('input[type=text]')
+const form = document.querySelector('.singup-form')
+const formInput = form.getElementsByTagName('input')
+console.log(formInput)
+console.log(allElementOne)
+const emailField = formInput[3]
+emailField.style.color = 'red'
+
+for (let index = 1; index < allElementOne.length; index++) {
+    const element = allElementOne[index];
+    console.log(element)
+    
+}
 

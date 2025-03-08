af2fc3b03cd1c527fa64bca906e7a827b1030880
SHOHEL HOSSAIN <developershohel@yahoo.com>
2025-03-08 19:47:55 +0600

    class nine

---
+++ base01/class-nine/portfolio/assets/js/script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll("nav ul li a").forEach(anchor => {
+    anchor.addEventListener("click", function(event) {
+        event.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({ behavior: "smooth" });
+    });
+});
